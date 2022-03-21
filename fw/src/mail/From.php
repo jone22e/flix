3b69155cf462528db1d9d6e9e4951539129d1bf6
Jone <ti@brasiltec.ind.br>
2022-03-21 15:22:54 -0300

    datset

---
+++ fw/src/mail/From.php
@@ -0,0 +1,9 @@
+<?php
+
+
+namespace Flix\FW\Mail;
+
+
+class From {
+
+}
--- fw/src/mail/Mail.php
+++ fw/src/mail/Mail.php
@@ -7,9 +7,22 @@ namespace Flix\FW\Mail;
 class Mail {
 
     private $recipient;
+    /**
+     * @var \Flix\FW\Mail\From
+     */
+    private $fromObj;
+
+    /**
+     * @param \Flix\FW\Mail\From $from
+     * @return $this
+     */
+    public function from($from) {
+        $this->fromObj = $from;
+        return $this;
+    }
 
     public function to($email) {
-        $this->recipient  =$email;
+        $this->recipient = $email;
         return $this;
     }
 
